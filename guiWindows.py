b3bf9d89988f2573fc9f1dcc1bdbe9d4b637dc2b
Ivan Filatov <36341190+Introvert696@users.noreply.github.com>
2020-11-08 19:13:02 +0400

    v 2.0
    Add Gui Version

--- guiWindows.py
+++ guiWindows.py
@@ -1,6 +1,6 @@
 import string
 import random
-
+from tkinter import *
 
 #   GWJCG-7C73B-JV8MR-F7GWJ-MCYYG  5 символов 5 строк
 def randomLater():
@@ -19,18 +19,40 @@ def randomKey():
 
         i +=1
     goodKey = ''.join(keyArray)
-    # print(goodKey)
     return goodKey
 
 
-def WriteKey():
+def WriteKey(quantity):
     f = open('text.txt', 'a')
-    quantity = int(input("Введите количество ключей: "))
     b = 0
     while b < quantity:
         f.write(randomKey() + '\n')
         b+=1
     f.close()
-WriteKey()
-print("Done!")
+#quantity = int(input("Введите количество ключей: "))
+
+
 #/////////////////////////////////////////////////////////
+
+def get_elen(event):
+    s = ent.get()
+    s = int(s)
+    texting = WriteKey(s)
+    lab['text'] = s , '- Ключ(а,ей) сгенерированно '
+
+
+# ................................................
+root = Tk()
+
+ent = Entry(width=20)
+but = Button(text="Преобразовать")
+lab = Label(width=30,height=1, bg='black', fg='white')
+
+# temtemp = int(ent)
+
+but.bind('<Button-1>',get_elen)
+
+ent.pack()
+but.pack()
+lab.pack()
+root.mainloop()
